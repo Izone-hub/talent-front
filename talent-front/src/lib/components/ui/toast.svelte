bd787ba6a82baef9521953a8a8ba3f2d71f5dfff
natnaelTy <natitaye316@gmail.com>
2026-07-03 16:59:21 +0300

    Increase z-index of Toast component for improved visibility

--- talent-front/src/lib/components/ui/toast.svelte
+++ talent-front/src/lib/components/ui/toast.svelte
@@ -3,7 +3,7 @@
 </script>
 
 {#if $toast.show}
-    <div class="toast toast-top toast-end z-[100]">
+    <div class="toast toast-top toast-end z-[9999]">
         <div
             class="alert text-base-content"
             class:alert-success={$toast.type === "success"}
